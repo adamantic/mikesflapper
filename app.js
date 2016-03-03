dd95afe21e992243829b0cc545980dd84faec783
michaelfrazis <michaelfrazis@gmail.com>
2016-03-04 09:33:11 +1100

    now working
    some bugs to fix!

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ var app = express();
 var mongoose = require('mongoose');
 var passport = require('passport');
 
+// connect MongoDB
 mongoose.connect('mongodb://localhost/news', function(err,db){
-    if(!err){
-        console.log('connected to /news');
+    if (!err){
+        console.log('Connected to /news!');
     } else{
-        console.dir(err); // failed to connect
+        console.dir(err); //failed to connect
     }
 });
 
@@ -26,8 +27,6 @@ require('./config/passport');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -36,15 +35,16 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
@@ -55,7 +55,7 @@ app.use(function (req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
+    app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -66,7 +66,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
@@ -74,4 +74,7 @@ app.use(function (err, req, res, next) {
     });
 });
 
+
 module.exports = app;
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,26 @@
-var express = require('express');
-var jwt = require('express-jwt');
-var router = express.Router();
-var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
-
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-
 var mongoose = require('mongoose');
+var express = require('express');
+var router = express.Router();
 var passport = require('passport');
+var jwt = require('express-jwt');
+
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 var User = mongoose.model('User');
 
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
+
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts){
-    if(err){ return next(err); }
+    if(err){ 
+      return next(err);
+    }
 
     res.json(posts);
   });
@@ -40,15 +42,27 @@ router.param('post', function(req, res, next, id) {
 
   query.exec(function (err, post){
     if (err) { return next(err); }
-    if (!post) { return next(new Error('cant find post')); }
+    if (!post) { return next(new Error("can't find post")); }
 
     req.post = post;
     return next();
   });
 });
 
+router.param('comment', function(req, res, next, id) {
+  var query = Comment.findById(id);
+
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error("can't find comment")); }
+
+    req.comment = comment;
+    return next();
+  });
+});
+
 router.get('/posts/:post', function(req, res, next) {
-  req.post.populate('comments',function(err,post){
+  req.post.populate('comments', function(err, post) {
     res.json(post);
   });
 });
@@ -61,35 +75,30 @@ router.put('/posts/:post/upvote', auth, function(req, res, next) {
   });
 });
 
-router.post('/posts/:post/comments', auth, function(req, res, next){
+router.put('/posts/:post/downvote', auth, function(req, res, next) {
+  req.post.downvote(function(err, post){
+    if (err) { return next(err); }
+
+    res.json(post);
+  });
+});
+
+router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
   comment.author = req.payload.username;
-
+  
   comment.save(function(err, comment){
-    if(err){return next(err);}
+    if(err){ return next(err); }
 
     req.post.comments.push(comment);
-    req.post.save(function(err, post){
-      if(err){return next(err);}
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
 
       res.json(comment);
     });
   });
 });
-
-router.param('comment', function(req, res, next, id) {
-  var query = Comment.findById(id);
-
-  query.exec(function (err, comment){
-    if (err) { return next(err); }
-    if (!post) { return next(new Error('can\'t find comment')); }
-
-    req.comment = comment;
-    return next();
-  });
-});
-
 
 router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
   req.comment.upvote(function(err, comment){
@@ -99,8 +108,16 @@ router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, nex
   });
 });
 
+router.put('/posts/:post/comments/:comment/downvote', auth, function(req, res, next) {
+  req.comment.downvote(function(err, comment){
+    if (err) { return next(err); }
+
+    res.json(comment);
+  });
+});
+
 router.post('/register', function(req, res, next){
-  if(!req.body.username|| !req.body.password){
+  if(!req.body.username || !req.body.password){
     return res.status(400).json({message: 'Please fill out all fields'});
   }
 
@@ -108,31 +125,30 @@ router.post('/register', function(req, res, next){
 
   user.username = req.body.username;
 
-  user.setPassword(req.body.password);
+  user.setPassword(req.body.password)
 
-  user.save(function(err){
-    if(err){return next(err);}
+  user.save(function (err){
+    if(err){ return next(err); }
+
     return res.json({token: user.generateJWT()})
   });
 });
 
 router.post('/login', function(req, res, next){
-  if(!req.body.username|| !req.body.password){
+  if(!req.body.username || !req.body.password){
     return res.status(400).json({message: 'Please fill out all fields'});
   }
 
+console.log('calling passport)');
   passport.authenticate('local', function(err, user, info){
-    if(err){return next(err);}
+    if(err){ return next(err); }
 
     if(user){
-      return res.json({ token: user.generateJWT()});
+      return res.json({token: user.generateJWT()});
     } else {
       return res.status(401).json(info);
     }
-
   })(req, res, next);
-})
+});
 
-
-
-module.exports = router; 
+module.exports = router;
