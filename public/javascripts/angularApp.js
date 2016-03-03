52b446dd2d08c347b1a9b8e65b6c747fa6197934
michaelfrazis <michaelfrazis@gmail.com>
2016-03-04 08:56:01 +1100

    Initial attempt, not working yet

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,233 @@
+var app = angular.module('mikesflapper',['ui.router']);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider,$urlRouterProvider){
+        $stateProvider
+         .state('home',{
+                url:'/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl',
+                resolve: {
+                    postPromise: ['posts', function(posts){
+                        return posts.getAll();
+                    }]
+                }          
+            })
+         .state('posts', {
+              url: '/posts/{id}',
+              templateUrl: '/posts.html',
+              controller: 'PostsCtrl',
+              resolve: {
+                post: ['$stateParams', 'posts', function($stateParams, posts) {
+                  return posts.get($stateParams.id);
+                }]
+              }
+            })
+         
+         .state('login',{
+          url: '/login',
+          templateUrl:'/login.html',
+          controller: 'AuthCtrl',
+          onEnter: ['$state','auth', function($state, auth){
+            if(auth.isLoggedIn()){
+              $state.go('home');
+            }
+          }]
+         })
+
+        .state('register',{
+          url: '/register',
+          templateUrl: '/register.html',
+          controller: 'AuthCtrl',
+          onEnter: ['$state', 'auth', function($state, auth){
+            if(auth.isLoggedIn()){
+              $state.go('home');
+            }
+          }]
+        });
+        $urlRouterProvider.otherwise('home');
+    }]);
+
+app.factory('posts', ['$http', 'auth', function($http){
+    var o = {
+        posts: []
+    };
+      o.getAll = function() {
+        return $http.get('/posts').success(function(data){
+          angular.copy(data, o.posts);
+        });
+      };
+    o.create = function(post) {
+      return $http.post('/posts', post, {
+        headers: {Authorization: 'Bearer '+auth.getToken()}
+      }).success(function(data){
+        o.posts.push(data);
+      });
+    };
+    o.upvote = function(post) {
+      return $http.put('/posts/' + post._id + '/upvote', null, {
+        headers: {Authorization: 'Bearer '+auth.getToken()}
+      }).success(function(data){
+        post.upvotes += 1;
+      });
+    };
+
+    o.get = function(id){
+      return $http.get('/posts/'+id).then(function(res){
+        return res.data;
+      });
+    };
+    o.addComment = function(id, comment) {
+      return $http.post('/posts/' + id + '/comments', comment, {
+        headers: {Authorization: 'Bearer '+auth.getToken()}
+      });
+    };
+    o.upvoteComment = function(post, comment) {
+      return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote', null, {
+        headers: {Authorization: 'Bearer '+auth.getToken()}
+      }).success(function(data){
+        comment.upvotes += 1;
+      });
+    };  
+
+    return o;
+}]);
+
+
+app.factory('auth',['$http', '$window', function($http, $window){
+  var auth = {};
+
+  auth.saveToken = function(token){
+    $window.localStorage['mikesflapper-token'] = token;
+  };
+
+  auth.getToken = function (){
+    return $window.localStorage['mikesflapper-token'];
+  };
+
+  auth.isLoggedIn = function(){
+    var token = auth.getToken();
+
+    if(token){
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+      return payload.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  };
+
+  auth.currentUser = function(){
+    if(auth.isLoggedIn()){
+      var token = auth.getToken();
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+      return payload.username;
+    }
+  };
+
+  auth.register = function(user){
+    return $http.post('/register', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logIn = function(user){
+    return $http.post('/login', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logout = function(){
+    $window.localStorage.removeItem('mikesflapper-token');
+  };
+
+  return auth;
+}]);
+
+app.controller('MainCtrl',[
+    '$scope','posts', 'auth', function($scope,posts){
+        $scope.posts=posts.posts;
+        $scope.test='hello world';
+        $scope.posts = [
+            {title:'post 1',upvotes:5},
+            {title:'post 2',upvotes:2},
+            {title:'post 3',upvotes:3},
+            {title:'post 4',upvotes:5},
+            {title:'post 5',upvotes:6},
+            {title:'post 6',upvotes:7}
+    ];
+        $scope.addPost = function(){
+          if(!$scope.title || $scope.title === '') { return; }
+          posts.create({
+            title: $scope.title,
+            link: $scope.link,
+          });
+          $scope.title = '';
+          $scope.link = '';
+        };
+        $scope.incrementUpvotes = function(post){
+            post.upvote(post);
+        };
+        $scope.addComment = function(){
+          if($scope.body ===''){return;}
+          posts.addComment(post._id,{
+            body: $scope.body,
+            author: 'user',
+          }).success(function(comment){
+            $scope.post.comments.push(comment);
+          });
+          $scope.body='';
+        }
+        $scope.isLoggedIn = auth.isLoggedIn;
+    } 
+]);
+
+app.controller('PostsCtrl',[
+    '$scope',
+    '$posts',
+    'post',
+    'auth',
+    function($scope, posts, post){
+        $scope.post = post;
+        $scope.incrementUpvotes=function(comment){
+          posts.upvoteComment(post,comment);
+        };
+        $scope.isLoggedIn = auth.isLoggedIn;
+    }
+])
+
+.controller('AuthCtrl', [
+'$scope',
+'$state',
+'auth',
+function($scope, $state, auth){
+  $scope.user = {};
+
+  $scope.register = function(){
+    auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+
+  $scope.logIn = function(){
+    auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+}])
+
+app.controller('NavCtrl',[
+  '$scope',
+  'auth',
+  function($scope, auth){
+    $scope.isLoggedIn = auth.isLoggedIn;
+    $scope.currentUser = auth.currentUser;
+    $scope.logOut = auth.logOut;
+  }]);
