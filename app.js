80a143cae96b85d718feb966de55f65f211bbee6
michaelfrazis <michaelfrazis@gmail.com>
2016-03-06 19:27:28 +1100

    all working!
    All working …

--- app.js
+++ app.js
@@ -1,61 +1,53 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var app = express();
-
 var mongoose = require('mongoose');
 var passport = require('passport');
 
-// connect MongoDB
-mongoose.connect('mongodb://localhost/news', function(err,db){
-    if (!err){
-        console.log('Connected to /news!');
-    } else{
-        console.dir(err); //failed to connect
-    }
-});
+mongoose.connect('mongodb://localhost/news');
 
 require('./models/Posts');
 require('./models/Comments');
 require('./models/Users');
+
 require('./config/passport');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(passport.initialize());
 
 app.use('/', routes);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
+/// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
 
-// error handlers
+/// error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
+        console.log(err.stack)
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -76,5 +68,3 @@ app.use(function(err, req, res, next) {
 
 
 module.exports = app;
-
-
